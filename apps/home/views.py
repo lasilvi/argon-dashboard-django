ec653b77fae8fc4b17683e4d66bb6072115e893d
Silvania Ramos Paternina <113859946+lasilvi@users.noreply.github.com>
2024-11-04 15:36:09 -0500

    segundo commit
    segundo commmit

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+import os
+from django.conf import settings
 
 
 @login_required(login_url="/login/")
@@ -31,14 +33,23 @@ def pages(request):
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
 
-        html_template = loader.get_template('home/' + load_template)
+         # Genera una lista de posibles rutas en subcarpetas
+        template_paths = [
+            os.path.join(dirpath, load_template)
+            for dirpath, _, filenames in os.walk(settings.TEMPLATES[0]['DIRS'][0])
+            if load_template in filenames
+        ]
+
+        # Intenta cargar el primer template encontrado
+        if template_paths:
+            html_template = loader.get_template(template_paths[0])
+        else:
+            # Si no se encuentra el archivo, renderiza la página 404
+            html_template = loader.get_template('home/page-404.html')
+
         return HttpResponse(html_template.render(context, request))
 
-    except template.TemplateDoesNotExist:
-
-        html_template = loader.get_template('home/page-404.html')
-        return HttpResponse(html_template.render(context, request))
-
-    except:
+    except Exception:
+        # Maneja cualquier otro error cargando una página 500
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
